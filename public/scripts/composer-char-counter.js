f9e079b197f5e414c98ebd21778f7f5475f31c05
RyanJohnson1612 <ryanjohnson@rogers.com>
2021-12-14 20:31:55 +0000

    Implement tweet character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // Update tweet char count on textarea input
+  $('#tweet-text').on('input', () => {
+    let tweetLength = $('#tweet-text').val().length;
+    let charsLeft = 140 - tweetLength;
+    $('.counter').text(charsLeft);
+
+    // Add tweet-error class if tweet is over 140 characters, remove it when less than 140 
+    if(charsLeft < 0) {
+      $('.counter').addClass('tweet-error');
+    } else {
+      $('.counter').removeClass('tweet-error');
+    }
+  })
+});
